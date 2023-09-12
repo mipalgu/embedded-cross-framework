35219bf522a064018462eaa06c02786decafa42a
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2023-09-12 14:05:04 +1000

    Added preset and toolchain for fpga.

---
+++ toolchains/fpga-xilinx-unknown-vivado/toolchain.cmake
@@ -0,0 +1,15 @@
+# Executable file extension
+IF(WIN32)
+    SET(TOOLCHAIN_EXE ".exe")
+ELSE()
+    SET(TOOLCHAIN_EXE "")
+ENDIF()
+
+# The triplet to use for the target
+SET(TARGET_TRIPLET "fpga-xilinx-unknown")
+
+find_program(TOOLCHAIN_COMPILER "vivado${TOOLCHAIN_EXE}")
+
+set(CMAKE_FPGA_COMPILER ${TOOLCHAIN_COMPILER})
+set(CMAKE_FPGA_FLAGS -mode tcl -source)
+

30cb794b2a7d1a3cc5635b5450f00ce57c5f9c4b
Morgan McColl <morgan.mccoll92@gmail.com>
2024-03-19 13:57:17 +1000

    Removed unnecessary variables.

--- projects/fpga/EmptyProject/project.cmake
+++ projects/fpga/EmptyProject/project.cmake
@@ -2,3 +2,5 @@ cmake_minimum_required(VERSION 3.12)
 project(EmptyProject LANGUAGE NONE)
 
 set(EmptyProject_PROJECT_DIRECTORY vivado_project)
+
+set(${CMAKE_PROJECT_NAME}_PROJECT_DIRECTORY ${PROJECT_SOURCE_DIR}/vivado_project)

80d139bac9e17770a7bcf8b89a62531f04305fc7
Morgan McColl <morgan.mccoll92@gmail.com>
2024-03-19 13:38:35 +1000

    Add check in project files.

--- toolchains/fpga-xilinx-unknown-vivado/toolchain.cmake
+++ toolchains/fpga-xilinx-unknown-vivado/toolchain.cmake
@@ -1,6 +1,6 @@
 # Executable file extension
 IF(WIN32)
-    SET(TOOLCHAIN_EXE ".exe")
+SET(TOOLCHAIN_EXE ".exe")
 ELSE()
     SET(TOOLCHAIN_EXE "")
 ENDIF()
@@ -8,7 +8,12 @@ ENDIF()
 # The triplet to use for the target
 SET(TARGET_TRIPLET "fpga-xilinx-unknown")
 
-find_program(TOOLCHAIN_COMPILER "vivado${TOOLCHAIN_EXE}")
+if (DEFINED VIVADO_BIN)
+    set(TOOLCHAIN_COMPILER ${VIVADO_BIN})
+else()
+    find_program(TOOLCHAIN_COMPILER "vivado${TOOLCHAIN_EXE}")
+endif()
+MESSAGE(STATUS "TOOLCHAIN_COMPILER: " ${TOOLCHAIN_COMPILER})
 
 set(CMAKE_FPGA_COMPILER ${TOOLCHAIN_COMPILER})
 set(CMAKE_FPGA_FLAGS -mode tcl -source)

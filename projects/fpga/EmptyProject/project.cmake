b959b9e78c2d85da8bcd9d16316e795d32848c3b
Morgan McColl <morgan.mccoll92@gmail.com>
2024-03-19 14:28:45 +1000

    Commented out unused variables.

--- projects/fpga/EmptyProject/project.cmake
+++ projects/fpga/EmptyProject/project.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.12)
 project(EmptyProject LANGUAGE NONE)
 
-set(EmptyProject_PROJECT_DIRECTORY vivado_project)
+# set(EmptyProject_PROJECT_DIRECTORY vivado_project)
 
-set(${CMAKE_PROJECT_NAME}_PROJECT_DIRECTORY ${PROJECT_SOURCE_DIR}/vivado_project)
+# set(${CMAKE_PROJECT_NAME}_PROJECT_DIRECTORY ${PROJECT_SOURCE_DIR}/vivado_project)

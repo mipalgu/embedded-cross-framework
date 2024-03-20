e9779639bf4b96d810c6e3e1a1e644d5f0fbd624
Morgan McColl <morgan.mccoll92@gmail.com>
2024-03-21 07:44:25 +1000

    Keep directory variable commented.

--- projects/fpga/EmptyProject/project.cmake
+++ projects/fpga/EmptyProject/project.cmake
@@ -1,6 +1,4 @@
 cmake_minimum_required(VERSION 3.12)
 project(EmptyProject LANGUAGES HDL)
 
-# set(EmptyProject_PROJECT_DIRECTORY vivado_project)
-
-# set(${CMAKE_PROJECT_NAME}_PROJECT_DIRECTORY ${PROJECT_SOURCE_DIR}/vivado_project)
+# set(${CMAKE_PROJECT_NAME}_PROJECT_DIRECTORY_NAME vivado_project)

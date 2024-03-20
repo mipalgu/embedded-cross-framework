dce3b3331b58fdef756722d92414ec5025eaf277
Morgan McColl <morgan.mccoll92@gmail.com>
2024-03-21 06:48:02 +1000

    Setting vivado locations.

--- toolchains/fpga-xilinx-unknown-vivado/toolchain.cmake
+++ toolchains/fpga-xilinx-unknown-vivado/toolchain.cmake
@@ -7,14 +7,20 @@ ENDIF()
 
 # The triplet to use for the target
 SET(TARGET_TRIPLET "fpga-xilinx-unknown")
-
-if (DEFINED VIVADO_BIN)
-    set(TOOLCHAIN_COMPILER ${VIVADO_BIN})
-else()
-    find_program(TOOLCHAIN_COMPILER "vivado${TOOLCHAIN_EXE}")
-endif()
-MESSAGE(STATUS "TOOLCHAIN_COMPILER: " ${TOOLCHAIN_COMPILER})
-
+set(HDL_BIN_PATH
+    /bin
+    /usr/bin
+    /usr/local/bin
+    /usr/local/tools/Xilinx/Vivado/2023.2/bin
+    /usr/local/tools/Xilinx/Vivado/2023.1/bin
+    /usr/local/tools/Xilinx/Vivado/2022.2/bin
+    /usr/local/tools/Xilinx/Vivado/2022.1/bin
+    /tools/Xilinx/Vivado/2023.2/bin
+    /tools/Xilinx/Vivado/2023.1/bin
+    /tools/Xilinx/Vivado/2022.2/bin
+    /tools/Xilinx/Vivado/2022.1/bin
+)
+find_program(TOOLCHAIN_COMPILER "vivado${TOOLCHAIN_EXE}" PATHS ${HDL_BIN_PATH})
 set(CMAKE_FPGA_COMPILER ${TOOLCHAIN_COMPILER})
 set(CMAKE_FPGA_FLAGS -mode tcl -source)
 set(${TARGET_TRIPLET}_USE_FPGA yes)

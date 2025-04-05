3339d7460aeed7f9f7bb235199b385e04fde9626
Rene Hexel <r.hexel@griffith.edu.au>
2025-04-05 13:55:03 +1000

    Add stm32f7xx support.

---
+++ boards/STM/nucleo_f756zg/include/hal.h
@@ -0,0 +1,6 @@
+#ifndef NUCLEO_F756ZG_HAL_H_
+#define NUCLEO_F756ZG_HAL_H_
+
+#include "stm32f7xx_hal.h"
+
+#endif
---
+++ boards/STM/nucleo_f756zg/include/ll.h
@@ -0,0 +1,30 @@
+#ifndef NUCLEO_F756ZG_LL_H_
+#define NUCLEO_F756ZG_LL_H_
+
+#include "stm32f7xx_ll_adc.h"
+#include "stm32f7xx_ll_bus.h"
+#include "stm32f7xx_ll_cortex.h"
+#include "stm32f7xx_ll_crc.h"
+#include "stm32f7xx_ll_dac.h"
+#include "stm32f7xx_ll_dma.h"
+#include "stm32f7xx_ll_dma2d.h"
+#include "stm32f7xx_ll_exti.h"
+#include "stm32f7xx_ll_fmc.h"
+#include "stm32f7xx_ll_gpio.h"
+#include "stm32f7xx_ll_i2c.h"
+#include "stm32f7xx_ll_iwdg.h"
+#include "stm32f7xx_ll_lptim.h"
+#include "stm32f7xx_ll_pwr.h"
+#include "stm32f7xx_ll_rcc.h"
+#include "stm32f7xx_ll_rng.h"
+#include "stm32f7xx_ll_rtc.h"
+#include "stm32f7xx_ll_sdmmc.h"
+#include "stm32f7xx_ll_spi.h"
+#include "stm32f7xx_ll_system.h"
+#include "stm32f7xx_ll_tim.h"
+#include "stm32f7xx_ll_usart.h"
+#include "stm32f7xx_ll_usb.h"
+#include "stm32f7xx_ll_utils.h"
+#include "stm32f7xx_ll_wwdg.h"
+
+#endif // NUCLEO_F756ZG_LL_H_

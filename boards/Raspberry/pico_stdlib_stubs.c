8178e83c6218bf75dfb1600bbfb56ff7653cbbe8
Rene Hexel <r.hexel@griffith.edu.au>
2025-04-25 08:40:10 +1000

    Add more stubs.

--- boards/Raspberry/pico_stdlib_stubs.c
+++ boards/Raspberry/pico_stdlib_stubs.c
@@ -7,3 +7,7 @@ __attribute__((weak)) int _fstat(int fd, struct stat *st) { (void)fd; (void)st; 
 __attribute__((weak)) int _isatty(int fd) { (void)fd; return 0; }
 __attribute__((weak)) int _kill(int pid, int sig) { (void)pid; (void)sig; return -1; }
 __attribute__((weak)) int _getpid(void) { return 1; }
+__attribute__((weak)) int _close(int fd) { (void)fd; return -1; }
+__attribute__((weak)) int _lseek(int fd, int ptr, int dir) { (void)fd; (void)ptr; (void)dir; return -1; }
+__attribute__((weak)) int _read(int fd, void *buf, size_t cnt) { (void)fd; (void)buf; (void)cnt; return -1; }
+__attribute__((weak)) int _write(int fd, const void *buf, size_t cnt) { (void)fd; (void)buf; (void)cnt; return -1; }
